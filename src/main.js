d559acf7365055e89b2595fecd354e5c1267f6d4
main <danila.kazakov451@icloud.com>
2024-10-11 12:09:45 +0300

    V3

--- src/main.js
+++ src/main.js
@@ -8,14 +8,23 @@ class App {
     this.filteredPosts = [];
     this.rootElement = document.getElementById('app');
 
-    this.postList = new PostList(this.handleDeletePost.bind(this), this.rootElement); 
-    this.search = new Search(this.handleSearch.bind(this), this.rootElement); 
+    this.postList = new PostList(this.handleDeletePost.bind(this), this.rootElement);
+    this.search = new Search(this.handleSearch.bind(this), this.rootElement);
   }
 
-  loadPosts() {
-    this.posts = this.getPosts();
-    this.filteredPosts = [...this.posts];
-    this.postList.render(this.filteredPosts);
+  // Асинхронная загрузка постов из JSON файла
+  async loadPosts() {
+    try {
+      const response = await fetch('./posts.json'); // Загрузка из posts.json
+      if (!response.ok) {
+        throw new Error('Ошибка загрузки данных');
+      }
+      this.posts = await response.json();
+      this.filteredPosts = [...this.posts];
+      this.postList.render(this.filteredPosts);
+    } catch (error) {
+      console.error('Ошибка при загрузке постов:', error);
+    }
   }
 
   handleSearch(searchText) {
@@ -28,63 +37,10 @@ class App {
   handleDeletePost(id) {
     this.posts = this.posts.filter(post => post.id !== id);
     this.filteredPosts = this.filteredPosts.filter(post => post.id !== id);
-    this.postList.render(this.filteredPosts); 
+    this.postList.render(this.filteredPosts);
   }
 
-  getPosts() {
-    return [
-      {
-        id: 1,
-        name: 'Первый пост',
-        body: 'Это содержимое первого поста. Здесь вы можете узнать много интересного!'
-      },
-      {
-        id: 2,
-        name: 'Второй пост',
-        body: 'Во втором посте мы обсуждаем различные темы. Не пропустите!'
-      },
-      {
-        id: 3,
-        name: 'Третий пост',
-        body: 'Третий пост посвящен новым технологиям и инновациям в нашей жизни.'
-      },
-      {
-        id: 4,
-        name: 'Четвёртый пост',
-        body: 'Здесь мы поговорим о личном развитии и самообразовании.'
-      },
-      {
-        id: 5,
-        name: 'Пятый пост',
-        body: 'В пятом посте мы рассмотрим увлекательные факты о мире.'
-      },
-      {
-        id: 6,
-        name: 'Шестой пост',
-        body: 'Шестой пост посвящён советам по улучшению продуктивности.'
-      },
-      {
-        id: 7,
-        name: 'Седьмой пост',
-        body: 'В этом посте мы обсудим самые популярные книги года.'
-      },
-      {
-        id: 8,
-        name: 'Восьмой пост',
-        body: 'Этот пост будет о путешествиях и лучших местах для отдыха.'
-      },
-      {
-        id: 9,
-        name: 'Девятый пост',
-        body: 'Девятый пост расскажет о здоровом образе жизни и правильном питании.'
-      },
-      {
-        id: 10,
-        name: 'Десятый пост',
-        body: 'В заключительном посте мы подведем итоги и сделаем выводы.'
-      }
-    ];
-  }
+  
 }
 
 const app = new App();
