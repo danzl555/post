466dc07a0e4fc1a13880e13055a01736253f1993
main <danila.kazakov451@icloud.com>
2024-10-11 17:12:48 +0300

    v4

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,21 +2,23 @@ import Post from './Post.js';
 
 class PostList {
   constructor(onDelete, rootElement) {
-    this.onDelete = onDelete; 
-    this.rootElement = rootElement; 
+    this.onDelete = onDelete;
+    this.rootElement = rootElement;
+    this.postListElement = document.createElement('div');
+    this.postListElement.classList.add('post-list');
   }
 
   render(posts) {
-    const postListElement = document.createElement('div');
-    postListElement.classList.add('post-list'); 
+    this.postListElement.innerHTML = '';
 
     posts.forEach(post => {
       const postComponent = new Post(post, this.onDelete);
-      postListElement.appendChild(postComponent.render());
+      this.postListElement.appendChild(postComponent.render());
     });
 
-    this.rootElement.querySelector('.post-list')?.remove();
-    this.rootElement.appendChild(postListElement);
+    if (!this.rootElement.contains(this.postListElement)) {
+      this.rootElement.appendChild(this.postListElement);
+    }
   }
 }
 
